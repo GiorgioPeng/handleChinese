942df70cce1a13058cf18fdecc6918e36ccbd7c2
GiorgioPeng <84867579@qq.com>
2020-01-19 16:47:24 +0000

    Deploying to gh-pages from master 1616a5cc3bd16480683b03f1d14f848be738a959

---
+++ precache-manifest.a7f5ac7396b3382e2bdbd2ed41c0ff1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb5d36ddb87b7b024f73ac70e779c5d",
+    "url": "/handleChinese/index.html"
+  },
+  {
+    "revision": "bb4d316eaa18484e2d1e",
+    "url": "/handleChinese/static/css/2.75a81f28.chunk.css"
+  },
+  {
+    "revision": "8ad2d2585e18e55de0f6",
+    "url": "/handleChinese/static/css/main.fbaae809.chunk.css"
+  },
+  {
+    "revision": "bb4d316eaa18484e2d1e",
+    "url": "/handleChinese/static/js/2.4f719538.chunk.js"
+  },
+  {
+    "revision": "05804a497ee95b83136b8e9bc6d41333",
+    "url": "/handleChinese/static/js/2.4f719538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ad2d2585e18e55de0f6",
+    "url": "/handleChinese/static/js/main.9172860b.chunk.js"
+  },
+  {
+    "revision": "fd21002fc5f1cc2febf4",
+    "url": "/handleChinese/static/js/runtime-main.340016d5.js"
+  }
+]);
